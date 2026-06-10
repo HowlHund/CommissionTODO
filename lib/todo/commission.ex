d22a5460f009c8fc094e0ea65cc08f4bb21ad148
chrys.nelson <chrys.nelson@MacBookPro-1219.lan>
2026-06-10 14:21:14 -0600

    Added module attributes and make a fake email service for when comms are added, deleted, and updated.

--- lib/todo/commission.ex
+++ lib/todo/commission.ex
@@ -6,6 +6,11 @@ defmodule Todo.Commission do
     :deadline, :inserted_at, :updated_at]} #specifies which fields to include in the JSON response
     @primary_key {:id, :binary_id, autogenerate: true} #this is for our uuid
 
+    @fields [:customer, :type, :status, :notes, :deadline]
+    @required_fields [:customer, :type, :status]
+    @valid_types ["full_body", "half_body", "head_shot", "ref", "chibi"]
+    @valid_statuses ["sketching", "lining", "coloring", "shading", "polishing", "completed", "cancelled"]
+
     schema "commissions" do #This is our schema for how a commission slot looks like in our "todo"
       field :customer, :string
       field :type, :string
@@ -18,9 +23,9 @@ defmodule Todo.Commission do
 
     def changeset(commission, attrs) do #this function validates a commission slot's attributes
       commission
-      |> cast(attrs, [:customer, :type, :status, :notes, :deadline]) #only accepts this pattern
-      |> validate_required([:customer, :type, :status]) #requires these fields to be present
-      |> validate_inclusion(:status, ["sketching", "lining", "coloring", "shading", "polishing","completed", "cancelled"]) #status must be one of these values
-      |> validate_inclusion(:type, ["full_body", "half_body", "head_shot", "ref", "chibi"]) #type must be one of these values
+      |> cast(attrs, @fields) #only accepts this pattern
+      |> validate_required(@required_fields) #requires these fields to be present
+      |> validate_inclusion(:status, @valid_statuses) #status must be one of these values
+      |> validate_inclusion(:type, @valid_types) #type must be one of these values
     end
 end
